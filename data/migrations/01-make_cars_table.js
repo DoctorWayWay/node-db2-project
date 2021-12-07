d162f984b661d4c0856347857ac7454d7821e8c0
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-07 12:35:21 -0800

    added initial migration

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,22 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
-};
+exports.up = async function (knex) {
+  return await knex.schema
+    .createTable("cars", table => {
+      table.increments("car_id")
+      table.string("vin")
+        .unique()
+        .notNullable()
+      table.string("make")
+        .notNullable()
+      table.string("model")
+        .notNullable()
+      table.decimal("mileage")
+        .notNullable()
+      table.string("title")
+      table.string("transmission")
+    })
+}
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
-};
+exports.down = async function (knex) {
+  return await knex.schema
+    .dropTableIfExists("cars")
+}
